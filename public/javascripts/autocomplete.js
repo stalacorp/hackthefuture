25e9f461a4de1eeb2cad3e0b9b5611e70a272299
haverra <birtpeeters@hotmail.com>
2015-12-03 10:16:58 +0100

    Routes

---
+++ public/javascripts/autocomplete.js
@@ -0,0 +1,43 @@
+$(function() {
+    alert('test');
+});
+
+var placeSearch, autocomplete;
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+        {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+// [START region_fillform]
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+
+    for (var component in componentForm) {
+        document.getElementById(component).value = '';
+        document.getElementById(component).disabled = false;
+    }
+}
+
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
--- public/javascripts/routes.js
+++ public/javascripts/routes.js
@@ -1,11 +1,44 @@
 var app = angular.module('Routes', ['ngResource', 'ngRoute']);
 
+$(function() {
+
+    alert('test');
+
+});
+
+
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'routes/address.html'
+            templateUrl: 'routes/address.html',
+            controller: 'PlanCtrl'
+
         })
         .otherwise({
             redirectTo: '/'
         });
 }]);
+
+app.controller('PlanCtrl', ['$scope', '$resource', '$location',
+    function($scope, $resource, $location){
+
+        $scope.geolocate = function () {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var geolocation = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    var circle = new google.maps.Circle({
+                        center: geolocation,
+                        radius: position.coords.accuracy
+                    });
+                    autocomplete.setBounds(circle.getBounds());
+                });
+            }
+        }
+
+
+
+
+    }]);
